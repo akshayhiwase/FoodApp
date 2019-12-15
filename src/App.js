86a5ea550148fe1965f8ddda6e61353c323b8ebb
Raghav <raghavendra.sao@mfine.co>
2019-12-15 14:09:59 +0530

    seach created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Counter from './Components/Counter';
+import React, { Component } from 'react';
+import MenuContainer from './Components/MenuContainer';
+import SeachBar from './Components/SearchBar';
+import logo from './images/logo192.png';
+
 import './App.css';
-import CounterContainer from './CounterContainer';
-import MenuContainer from './Components/MenuContainer'
 const data = [{
   heading: "Counter 1",
   inititalCounter: 100,
@@ -11,24 +12,139 @@ const data = [{
   heading: "Counter 2",
   inititalCounter: 40
 }]
-function App() {
-  return (
-    <div className="App">
-      {
-        data.map(value => (
-          <div>
-            <div>{value.heading}</div>
-            <Counter inititalCounter={value.inititalCounter}/>
-          </div>
-          )
-        )
 
-      }
-      <MenuContainer />
+const initialMenuData = [
+  {
+    id: 1,
+    name: 'Pizza',
+    price: 55,
+    ratting: 3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+  },
+  {
+    id: 2,
+    price: 100,
+    name: 'pasta',
+    ratting: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: logo
+  },
+  {
+    id: 3,
+    name: 'Pizza',
+    price: 55,
+    ratting: 3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+  },
+  {
+    id: 4,
+    price: 100,
+    name: 'pasta',
+    ratting: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: logo
+  },
+  {
+    id: 5,
+    name: 'Pizza',
+    price: 55,
+    ratting: 3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+  },
+  {
+    id: 6,
+    price: 100,
+    name: 'pasta',
+    ratting: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: logo
+  },
+  {
+    id: 7,
+    name: 'Pizza',
+    price: 55,
+    ratting: 3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+  },
+  {
+    id: 8,
+    price: 100,
+    name: 'pasta',
+    ratting: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: logo
+  },
+  {
+    id: 9,
+    name: 'Pizza',
+    price: 55,
+    ratting: 3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+  },
+  {
+    id: 10,
+    price: 100,
+    name: 'pasta',
+    ratting: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: logo
+  },
+  {
+    id: 11,
+    name: 'Pizza',
+    price: 55,
+    ratting: 3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+  },
+  {
+    id: 12,
+    price: 100,
+    name: 'pasta',
+    ratting: 5,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
+    imageUrl: logo
+  },
 
-      {/* <CounterContainer inititalCounter={50}/> */}
-    </div>
-  );
+]
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filteredMenu: initialMenuData,
+        }
+    }
+
+    setSearchText = text => {
+        const filteredMenu = initialMenuData.filter(menu => menu.name.includes(text));
+        this.setState({
+            ...this.state,
+            filteredMenu: filteredMenu
+        });
+    }
+
+    setSearchByRatting = text => {
+        const filteredMenu = initialMenuData.filter(menu => menu.ratting >= parseInt(text));
+        this.setState({
+            ...this.state,
+            filteredMenu: filteredMenu
+        });
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <SeachBar setSearchText={this.setSearchText} setSearchByRatting={this.setSearchByRatting}/>
+        <MenuContainer menuData={this.state.filteredMenu} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/MenuContainer/index.js
+++ src/Components/MenuContainer/index.js
@@ -1,51 +1,31 @@
 import React from 'react';
 
-import logo from '../../images/logo192.png';
 
+import './style.css';
 
-const menuData = [
-    {
-        id: 1,
-        name: 'Pizza',
-        price: 55,
-        ratting: 3,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
-        imageUrl: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
-    },
-    {
-        id: 2,
-        price: 100,
-        name: 'pasta',
-        ratting: 5,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolore, quas voluptatem suscipit',
-        imageUrl: logo
-    }
-]
-
-
-const MenuContainer = () => {
+const MenuContainer = (props) => {
     return (
-        <div>
-            {true ? "hello" : "hi"}
-            {menuData.map(menu => (
-                <div class="menu" style={ {border: "1px solid #ccc", marginBottom: "10px", padding: "20px"} } >        
+        <div id="menu-container">
+            {props.menuData.map(menu => (
+                <div className="menu" style={ {border: "1px solid #ccc", marginBottom: "10px", padding: "20px"} } key={menu.id} >   
+                    <div>{menu.id}</div>     
                     <img src={menu.imageUrl} alt={`${menu.name}'s image`}
-                        style={ {width: "100px"} } />
+                        style={ {width: "100%"} } />
                     
-                    <div class="menu-description">
-                        <div class="name-price-ratting">
+                    <div className="menu-description">
+                        <div className="name-price-ratting">
                             <div>
-                                <span class="bold">{menu.name}</span>
-                                <div class="bold">
+                                <span className="bold">{menu.name}</span>
+                                <div className="bold">
                                     &#x20b9; {menu.price}
                                 </div>
                             </div>
-                            <div class="star">
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star"></span>
-                                <span class="fa fa-star"></span>
+                            <div className="star">
+                                <span className="fa fa-star checked"></span>
+                                <span className="fa fa-star checked"></span>
+                                <span className="fa fa-star checked"></span>
+                                <span className="fa fa-star"></span>
+                                <span className="fa fa-star"></span>
         
                             </div>
                         </div>
